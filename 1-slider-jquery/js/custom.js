287038747cfa561739b42346453737051d316d0b
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-24 17:04:41 +0200

    init

--- 1-slider-jquery/js/custom.js
+++ 1-slider-jquery/js/custom.js
@@ -1,6 +1,13 @@
 'use strict';
 
 $(function(){
+    /*Zmienne*/
+    var slideCount = $('.single-slide').length;
+    var slideShow = $('.slide-show');
+    
+    /* Szerokość kontenera*/
+    slideShow.css('width', slideCount * 100 + '%');
+    
     
     
     
