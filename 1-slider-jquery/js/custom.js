0ffeb482bca2a81367356ce9b5645c41f1912b73
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-25 11:25:57 +0200

    slider

--- 1-slider-jquery/js/custom.js
+++ 1-slider-jquery/js/custom.js
@@ -4,12 +4,52 @@ $(function(){
     /*Zmienne*/
     var slideCount = $('.single-slide').length;
     var slideShow = $('.slide-show');
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
     
     /* Szerokość kontenera*/
     slideShow.css('width', slideCount * 100 + '%');
     
+    //szerokość i położenie pojedynczego slajdu
     
+    $('.single-slide').each(function(index){
+       
+        $(this).css({'width': slideWidth + '%', 'margin-left': index * slideWidth + '%'});
+        
+    });
     
+    //funkcja slide
+    function slide(newSlideIndex){
+        if (newSlideIndex<0 || newSlideIndex>=slideCount){
+//            newSlideIndex = 0;
+            return;
+        }
+        
+        var marginLeft = (newSlideIndex * (-100)) + '%';
+        
+//        console.log(marginLeft);
+        
+        slideShow.animate({'margin-left': marginLeft}, 800, function(){
+            
+            slideIndex = newSlideIndex;
+            
+        });
+    }
+    
+    //strzałka w lewo
+    $('.prev-slide').click(function(){
+    
+    slide(slideIndex-1)
+        
+    });
+        
+        
+    //strzałka w prawo
+    $('.next-slide').click(function(){
+                      
+    slide(slideIndex+1) 
+        
+    });
     
     
     
